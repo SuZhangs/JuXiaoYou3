75000e56264b38e5a2956c4db5cd619ea02a4320
Acoris <731984870@qq.com>
2023-02-17 12:42:04 +0800

    添加无头按钮

---
+++ ui/Forest/Forest.Controls/Controls/LooklessButton.cs
@@ -0,0 +1,10 @@
+﻿namespace Acorisoft.FutureGL.Forest.Controls
+{
+    public class LooklessButton : Button
+    {
+        static LooklessButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LooklessButton), new FrameworkPropertyMetadata(typeof(LooklessButton)));
+        }
+    }
+}
