39bcf84924b1f30208bd8eac04a71ad30d40efc7
Acoris <731984870@qq.com>
2023-02-18 12:27:09 +0800

    添加图表内容块

--- frameworks/MigaDB/Data/Templates/Module/ChartBlock.cs
+++ frameworks/MigaDB/Data/Templates/Module/ChartBlock.cs
@@ -1,26 +1,119 @@
-﻿namespace Acorisoft.FutureGL.MigaDB.Data.Templates.Module
+﻿using System.Collections.ObjectModel;
+
+namespace Acorisoft.FutureGL.MigaDB.Data.Templates.Module
 {
     /// <summary>
-    /// 表示选项内容块。
+    /// 表示图表内容块。
     /// </summary>
-    public interface IChartBlock : IModuleBlock, IModuleBlock<bool>
+    public interface IChartBlock : IModuleBlock, IModuleBlock<int[]>
     {
+        /// <summary>
+        /// 最大值
+        /// </summary>
+        int Maximum { get; }
+
+        /// <summary>
+        /// 最小值
+        /// </summary>
+        int Minimum { get; }
+        
+        /// <summary>
+        /// 轴
+        /// </summary>
+        string[] Axis { get; }
+        
+        /// <summary>
+        /// 颜色
+        /// </summary>
+        string Color { get; }
     }
 
     /// <summary>
-    /// 表示选项内容块。
+    /// 表示图表内容块。
     /// </summary>
     public interface IChartBlockDataUI : IChartBlock, IModuleBlockDataUI
     {
     }
 
     /// <summary>
-    /// 表示选项内容块。
+    /// 表示图表内容块。
     /// </summary>
-    public interface IChartBlockEditUI : IModuleBlockEditUI, IModuleBlockEditUI<bool>
+    public interface IChartBlockEditUI : IModuleBlockEditUI, IModuleBlockEditUI<int[]>
     {
+        /// <summary>
+        /// 轴
+        /// </summary>
+        ObservableCollection<string> Axis { get; }
+        
+        /// <summary>
+        /// 颜色
+        /// </summary>
+        public string Color { get; set; }
+        
+        /// <summary>
+        /// 最大值
+        /// </summary>
+        int Maximum { get; set; }
+
+        /// <summary>
+        /// 最小值
+        /// </summary>
+        int Minimum { get; set; }
     }
-    public class ChartBlock
+    
+    /// <summary>
+    /// 表示图表内容块。
+    /// </summary>
+    public abstract class ChartBlock : ModuleBlock, IChartBlock
+    {
+        /// <summary>
+        /// 清除当前值。
+        /// </summary>
+        public override void ClearValue() => Value = null;
+
+        /// <summary>
+        /// 最大值
+        /// </summary>
+        public int Maximum { get; init; }
+
+        /// <summary>
+        /// 最小值
+        /// </summary>
+        public int Minimum { get; init; }
+        
+        /// <summary>
+        /// 颜色
+        /// </summary>
+        public string Color { get; init; }
+        
+        /// <summary>
+        /// 当前值
+        /// </summary>
+        public int[] Value { get; set; }
+        
+        /// <summary>
+        /// 默认值
+        /// </summary>
+        public int[] Fallback { get; init; }
+        
+        /// <summary>
+        /// 轴
+        /// </summary>
+        public string[] Axis { get; init; }
+    }
+
+    /// <summary>
+    /// 表示雷达图内容块。
+    /// </summary>
+    public sealed class RadarBlock : ChartBlock
+    {
+        
+    }
+
+    /// <summary>
+    /// 表示柱形图内容块。
+    /// </summary>
+    public sealed class HistogramBlock : ChartBlock
     {
         
     }
---
+++ studio/SubSystem.Template/Modules/Chart.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Acorisoft.FutureGL.MigaDB.Data.Templates.Module;
+using DynamicData;
+// ReSharper disable SuggestBaseTypeForParameterInConstructor
+
+namespace Acorisoft.FutureGL.MigaStudio.Modules
+{
+    public abstract class ChartBlockDataUI : ModuleBlockDataUI<ChartBlock, int[]>, IChartBlockDataUI
+    {
+        protected ChartBlockDataUI(ChartBlock block) : base(block)
+        {
+            Axis    = block.Axis;
+            Color   = block.Color;
+            Maximum = block.Maximum;
+            Minimum = block.Minimum;
+        }
+
+        protected override int[] OnValueChanged(int[] oldValue, int[] newValue)
+        {
+            if (newValue is null || newValue.Length == 0)
+            {
+                newValue = Fallback;
+            }
+
+            for (var i = 0; i < newValue.Length; i++)
+            {
+                newValue[i] = Math.Clamp(newValue[i], Minimum, Maximum);
+            }
+
+            TargetBlock.Value = newValue;
+            return newValue;
+        }
+
+
+        /// <summary>
+        /// 最大值
+        /// </summary>
+        public int Maximum { get; }
+
+        /// <summary>
+        /// 最小值
+        /// </summary>
+        public int Minimum { get; }
+
+        public string[] Axis { get; }
+        public string Color { get; }
+    }
+
+    public abstract class ChartBlockEditUI : ModuleBlockEditUI<ChartBlock, int[]>, IChartBlockEditUI
+    {
+        private int    _maximum;
+        private int    _minimum;
+        private string _color;
+
+        protected ChartBlockEditUI(ChartBlock block) : base(block)
+        {
+            Axis    = new ObservableCollection<string>();
+            Maximum = block.Maximum;
+            Minimum = block.Minimum;
+            Color   = block.Color;
+            
+            if (block.Axis is not null)
+            {
+                Axis.AddRange(block.Axis);
+            }
+        }
+
+        /// <summary>
+        /// 获取或设置 <see cref="Color"/> 属性。
+        /// </summary>
+        public string Color
+        {
+            get => _color;
+            set => SetValue(ref _color, value);
+        }
+        /// <summary>
+        /// 最小值
+        /// </summary>
+        public int Minimum
+        {
+            get => _minimum;
+            set => SetValue(ref _minimum, value);
+        }
+
+        /// <summary>
+        /// 最大值
+        /// </summary>
+        public int Maximum
+        {
+            get => _maximum;
+            set => SetValue(ref _maximum, value);
+        }
+
+        public ObservableCollection<string> Axis { get; }
+    }
+
+    public class RadarBlockDataUI : ChartBlockDataUI
+    {
+        public RadarBlockDataUI(RadarBlock block) : base(block)
+        {
+        }
+    }
+    
+    public class HistogramBlockDataUI : ChartBlockDataUI
+    {
+        public HistogramBlockDataUI(HistogramBlock block) : base(block)
+        {
+        }
+    }
+    
+    public class RadarBlockEditUI : ChartBlockEditUI
+    {
+        public RadarBlockEditUI(RadarBlock block) : base(block)
+        {
+        }
+
+        protected override ChartBlock CreateInstanceOverride()
+        {
+            return new RadarBlock
+            {
+                Id       = Id,
+                Name     = Name,
+                Metadata = Metadata,
+                Fallback = Fallback,
+                ToolTips = ToolTips,
+                Maximum  = Maximum,
+                Minimum  = Minimum,
+                Axis     = Axis.ToArray(),
+                Color    = Color,
+            };
+        }
+    }
+    
+    public class HistogramBlockEditUI : ChartBlockEditUI
+    {
+        public HistogramBlockEditUI(HistogramBlock block) : base(block)
+        {
+        }
+        
+        protected override ChartBlock CreateInstanceOverride()
+        {
+            return new HistogramBlock
+            {
+                Id       = Id,
+                Name     = Name,
+                Metadata = Metadata,
+                Fallback = Fallback,
+                ToolTips = ToolTips,
+                Maximum  = Maximum,
+                Minimum  = Minimum,
+                Axis     = Axis.ToArray(),
+                Color    = Color,
+            };
+        }
+    }
+}
