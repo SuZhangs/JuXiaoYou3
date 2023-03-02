14452aca6e791d3d25806f58fbb16f6c367b0660
Acoris <731984870@qq.com>
2023-03-02 14:45:28 +0800

    save

--- studio/SubSystem.Template/Modules/ViewModels/ModuleViewModel.cs
+++ studio/SubSystem.Template/Modules/ViewModels/ModuleViewModel.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Acorisoft.FutureGL.Forest.ViewModels;
 using Acorisoft.FutureGL.MigaDB.Data.Templates.Module;
 using Acorisoft.FutureGL.MigaDB.Documents;
+using Acorisoft.FutureGL.MigaDB.Utils;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData;
 
 namespace Acorisoft.FutureGL.MigaStudio.Modules.ViewModels
 {
     public class ModuleViewModel : ViewModelBase
     {
+        private readonly Dictionary<string, int> _metadataMap;
+
         private string       _name;
         private string       _authorList;
         private string       _contractList;
@@ -20,8 +26,90 @@ namespace Acorisoft.FutureGL.MigaStudio.Modules.ViewModels
 
         public ModuleViewModel()
         {
-            MetadataList = new ObservableCollection<MetadataCache>();
-            Blocks       = new ObservableCollection<ModuleBlock>();
+            _metadataMap = new Dictionary<string, int>();
+            MetadataList  = new ObservableCollection<MetadataCache>();
+            Blocks        = new ObservableCollection<ModuleBlock>();
+        }
+
+        public void NewTemplate()
+        {
+            _name          = string.Empty;
+            _authorList    = string.Empty;
+            _contractList  = string.Empty;
+            _organizations = string.Empty;
+            _copyright     = string.Empty;
+            _intro         = string.Empty;
+            _for           = string.Empty;
+            _id            = ID.Get();
+            _version       = 1;
+            _forType       = DocumentType.CharacterRule;
+            _metadataMap.Clear();
+            MetadataList.Clear();
+            Blocks.Clear();
+        }
+
+        private void OpenTemplate(ModuleTemplate template)
+        {
+            _name          = template.Name;
+            _authorList    = template.AuthorList;
+            _contractList  = template.ContractList;
+            _organizations = template.Organizations;
+            _copyright     = template.Copyright;
+            _intro         = template.Intro;
+            _for           = template.For;
+            _id            = template.Id;
+            _version       = template.Version;
+            _forType       = template.ForType;
+            _metadataMap.Clear();
+            
+            foreach (var cache in template.MetadataList)
+            {
+                _metadataMap.Add(cache.Id, cache.RefCount);
+                MetadataList.Add(cache);
+            }
+            
+            MetadataList.Clear();
+            Blocks.Clear();
+            Blocks.AddRange(template.Blocks);
+        }
+
+        public void AddBlock(ModuleBlock block)
+        {
+            if (block is null)
+            {
+                return;
+            }
+
+            if (Blocks.Contains(block))
+            {
+                return;
+            }
+            
+            Blocks.Add(block);
+        }
+        
+        
+        public void EditBlock(ModuleBlock block)
+        {
+            if (block is null)
+            {
+                return;
+            }
+            
+            if (!Blocks.Contains(block))
+            {
+                return;
+            }
+        }
+
+        public void RemoveBlock(ModuleBlock block)
+        {
+            if (block is null)
+            {
+                return;
+            }
+            
+            Blocks.Remove(block);
         }
 
         /// <summary>
@@ -123,5 +211,15 @@ namespace Acorisoft.FutureGL.MigaStudio.Modules.ViewModels
         /// 模组内容块集合。
         /// </summary>
         public ObservableCollection<ModuleBlock> Blocks { get; init; }
+
+        public RelayCommand AddBlockCommand { get; }
+        public RelayCommand EditBlockCommand { get; }
+        public RelayCommand RemoveBlockCommand { get; }
+        public RelayCommand AddItemCommand { get; }
+        public RelayCommand EditItemCommand { get; }
+        public RelayCommand RemoveItemCommand { get; }
+        public RelayCommand SaveTemplateCommand { get; }
+        public RelayCommand OpenTemplateCommand { get; }
+        public RelayCommand NewTemplateCommand { get; }
     }
 }
