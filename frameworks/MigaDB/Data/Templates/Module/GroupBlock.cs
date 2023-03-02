6e9cbba6801504b53592f135356bd267d6bd66eb
Acoris <731984870@qq.com>
2023-03-02 11:59:41 +0800

    添加选项组内容块

---
+++ frameworks/MigaDB/Data/Templates/Module/GroupBlock.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Acorisoft.FutureGL.MigaDB.Data.Templates.Module
+{
+    /// <summary>
+    /// 表示两极内容块。
+    /// </summary>
+    public interface IGroupBlock : IModuleBlock
+    {
+        List<ModuleBlock> Items { get; }
+    }
+
+    /// <summary>
+    /// 表示两极内容块。
+    /// </summary>
+    public interface IGroupBlockDataUI : IGroupBlock, IModuleBlockDataUI
+    {
+    }
+
+    /// <summary>
+    /// 表示两极内容块。
+    /// </summary>
+    public interface IGroupBlockEditUI : IModuleBlockEditUI
+    {
+        ObservableCollection<ModuleBlock> Items { get; }
+    }
+
+    public class GroupBlock : ModuleBlock, IGroupBlock
+    {
+        protected override bool CompareTemplateOverride(ModuleBlock block)
+        {
+            if (block is not GroupBlock gb)
+            {
+                return false;
+            }
+
+            if (gb.Items.Count != Items.Count)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < Items.Count; i++)
+            {
+                var t = gb.Items[i];
+                var s = Items[i];
+
+                if (!s.CompareTemplate(t))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected override bool CompareValueOverride(ModuleBlock block)
+        {
+            if (block is not GroupBlock gb)
+            {
+                return false;
+            }
+
+            if (gb.Items.Count != Items.Count)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < Items.Count; i++)
+            {
+                var t = gb.Items[i];
+                var s = Items[i];
+
+                if (!s.CompareValue(t))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override void ClearValue()
+        {
+            foreach (var item in Items)
+            {
+                item.ClearValue();
+            }
+        }
+
+        public List<ModuleBlock> Items { get; init; }
+    }
+}
---
+++ studio/SubSystem.Template/Modules/Group.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Acorisoft.FutureGL.MigaDB.Data.Templates.Module;
+using DynamicData;
+
+namespace Acorisoft.FutureGL.MigaStudio.Modules
+{
+    public class GroupBlockDataUI : ModuleBlockDataUI, IGroupBlockDataUI
+    {
+
+        public GroupBlockDataUI(GroupBlock block) : base(block)
+        {
+            TargetBlock = block;
+            Items       = new List<ModuleBlock>();
+            if (block.Items is not null)
+            {
+                Items.AddRange(block.Items);
+            }
+        }
+
+        public override bool CompareTemplate(ModuleBlock block)
+        {
+            return TargetBlock.CompareTemplate(block);
+        }
+
+        public override bool CompareValue(ModuleBlock block)
+        {
+            return TargetBlock.CompareValue(block);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        protected GroupBlock TargetBlock { get; }
+
+        public List<ModuleBlock> Items { get; init; }
+    }
+    
+    public class GroupBlockEditUI : ModuleBlockEditUI, IGroupBlockEditUI
+    {
+        public GroupBlockEditUI(IGroupBlock block) : base(block)
+        {
+            Items       = new ObservableCollection<ModuleBlock>();
+            if (block.Items is not null)
+            {
+                Items.AddRange(block.Items);
+            }
+        }
+
+        public override ModuleBlock CreateInstance()
+        {
+            return new GroupBlock
+            {
+                Id       = Id,
+                Name     = Name,
+                Metadata = Metadata,
+                ToolTips = ToolTips,
+                Items    = new List<ModuleBlock>(Items)
+            };
+        }
+
+        public ObservableCollection<ModuleBlock> Items { get; init; }
+    }
+}
