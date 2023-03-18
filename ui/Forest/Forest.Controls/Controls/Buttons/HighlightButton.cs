5930508b14842fae07f6519ef7ddd23b7807652f
Acoris <731984870@qq.com>
2023-03-18 09:57:02 +0800

    移除部分不需要的Xaml

--- ui/Forest/Forest.Controls/Controls/Buttons/HighlightButton.cs
+++ ui/Forest/Forest.Controls/Controls/Buttons/HighlightButton.cs
@@ -5,23 +5,8 @@ using Acorisoft.FutureGL.Forest.Styles;
 
 namespace Acorisoft.FutureGL.Forest.Controls.Buttons
 {
-    public class HighlightButton : ForestButtonBase
+    public class HighlightButton
     {
-        static HighlightButton()
-        {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(HighlightButton), new FrameworkPropertyMetadata(typeof(HighlightButton)));
-        }
-
-        private const string PART_BdName      = "PART_Bd";
-        private const string PART_ContentName = "PART_Content";
-
-        private Border           _bd;
-        private ContentPresenter _content;
-
-        protected override void GetTemplateChildOverride(ITemplatePartFinder finder)
-        {
-            finder.Find<Border>(PART_BdName, x => _bd                     = x)
-                  .Find<ContentPresenter>(PART_ContentName, x => _content = x);
-        }
+        
     }
 }
