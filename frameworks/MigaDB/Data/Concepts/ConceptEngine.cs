35e03b7dececa9115753323cb514083e5e405ab0
Acoris <731984870@qq.com>
2023-03-31 10:55:37 +0800

    Create ConceptEngine.cs

---
+++ frameworks/MigaDB/Data/Concepts/ConceptEngine.cs
@@ -0,0 +1,15 @@
+﻿namespace Acorisoft.FutureGL.MigaDB.Data.Concepts
+{
+    public class ConceptEngine : DataEngine
+    {
+        protected override void OnDatabaseOpening(DatabaseSession session)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnDatabaseClosing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
