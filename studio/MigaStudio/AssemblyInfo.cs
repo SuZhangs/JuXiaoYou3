0000aeae92b9f9fbd366c13fe5c5abc4c905a02f
Acoris <731984870@qq.com>
2023-04-19 17:57:25 +0800

    修改了程序集标题

--- studio/MigaStudio/AssemblyInfo.cs
+++ studio/MigaStudio/AssemblyInfo.cs
@@ -4,13 +4,17 @@ global using Acorisoft.FutureGL.Forest.Styles;
 global using Acorisoft.FutureGL.Forest.ViewModels;
 
 global using Acorisoft.FutureGL.MigaStudio.ViewModels;
+using System.Reflection;
 using System.Windows;
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page,
-    // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page,
-    // app, or any theme specific resource dictionaries)
-)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyCompany("罗易斯（Luoyisi）")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyProduct("橘小柚")]
+[assembly: AssemblyTitle("橘小柚")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: System.Resources.NeutralResourcesLanguageAttribute("zh")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
