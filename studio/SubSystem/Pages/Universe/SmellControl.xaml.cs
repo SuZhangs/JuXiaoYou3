9e158a111d86a7045586843e54b19c2a066dede0
Acoris <731984870@qq.com>
2023-05-10 13:08:10 +0800

    完善植物部分的信息

---
+++ studio/SubSystem/Pages/Universe/SmellControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Acorisoft.FutureGL.MigaStudio.Pages.Universe
+{
+    public partial class SmellControl
+    {
+        public SmellControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ studio/SubSystem/Pages/Universe/TasteControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Acorisoft.FutureGL.MigaStudio.Pages.Universe
+{
+    public partial class TasteControl
+    {
+        public TasteControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
