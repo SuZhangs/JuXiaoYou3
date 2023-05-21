172cb6d30c0f8cd5a36dafdd129a547520fb4cbf
Acoris <731984870@qq.com>
2023-05-21 10:30:41 +0800

    save

--- studio/MigaStudio.UI/AssemblyInfo.cs
+++ studio/MigaStudio.UI/AssemblyInfo.cs
@@ -19,6 +19,7 @@ using System.Windows.Markup;
     // app, or any theme specific resource dictionaries)
 )]
 [assembly: XmlnsDefinition("urn:acorisoft/studio", "Acorisoft.FutureGL.MigaStudio.Controls")]
+[assembly: XmlnsDefinition("urn:acorisoft/studio", "Acorisoft.FutureGL.MigaStudio.Controls.Socials")]
 [assembly: XmlnsDefinition("urn:acorisoft/studio", "Acorisoft.FutureGL.MigaStudio.Resources.Behaviors")]
 [assembly: XmlnsDefinition("urn:acorisoft/studio", "Acorisoft.FutureGL.MigaStudio.Resources.Converters")]
 [assembly: XmlnsDefinition("urn:acorisoft/studio", "Acorisoft.FutureGL.MigaStudio.Resources.Panels")]
