ecf63f6ac9455e489c706cd27916a560713789f3
Acoris <731984870@qq.com>
2023-06-08 22:02:51 +0800

    修复添加音乐时因为确实标签中的歌手名单而无法添加

--- studio/SubSystem/Utilities/MusicUtilities.cs
+++ studio/SubSystem/Utilities/MusicUtilities.cs
@@ -8,9 +8,16 @@ namespace Acorisoft.FutureGL.MigaStudio.Utilities
 {
     public static class MusicUtilities
     {
-        private static string CombineString(IEnumerable<string> source)
+        private static string CombineString(IReadOnlyList<string> source)
         {
             const string pattern = "{0},{1}";
+
+            if (source is null ||
+                source.Count == 0)
+            {
+                return "Unknown";
+            }
+            
             return source.Aggregate((a, b) => string.Format(pattern, a, b));
         }
         
@@ -18,50 +25,7 @@ namespace Acorisoft.FutureGL.MigaStudio.Utilities
         {
             foreach (var fileName in fileNames)
             {
-                Music music;
-                var   fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
-                var   fileNameWithExtension    = fileNameWithoutExtension + ".mp3";
-
-                if (engine.HasFile(fileNameWithExtension))
-                {
-                    music = engine.GetFile(fileNameWithExtension);
-                }
-                else
-                {
-                    var file      = File.Create(fileName);
-                    var musicFile = (AudioFile)file;
-                    var tag       = musicFile.GetTag(TagTypes.Id3v2);
-                    var cover     = Path.GetFileNameWithoutExtension(fileName) + ".png";
-
-                    if (tag.Pictures is not null &&
-                        tag.Pictures?.Length > 0)
-                    {
-                        var pic = tag.Pictures.First();
-                        cover = Path.GetFileNameWithoutExtension(fileName) + ".png";
-                        await engine.WriteAlbum(pic.Data.Data, cover);
-                    }
-
-                    var title      = string.IsNullOrEmpty(tag.Title) ? fileNameWithoutExtension : tag.Title;
-                    var performers = CombineString(tag.Performers);
-                    var dst        = Path.Combine(engine.FullDirectory, fileNameWithExtension);
-
-                    System.IO.File.Copy(fileName, dst, true);
-                    
-                    music = new Music
-                    {
-                        Id     = fileNameWithExtension,
-                        Path   = dst,
-                        Name   = title,
-                        Author = performers,
-                        Cover  = cover
-                    };
-
-                    engine.AddFile(music);
-                }
-
-                //
-                //
-                callback(music);
+                await AddMusic(fileName, engine, callback);
             }
         }
         
