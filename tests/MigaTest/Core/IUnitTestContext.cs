1a174b81022f709b3dc41df83a993da6b4a11831
Acoris <731984870@qq.com>
2023-06-11 17:22:43 +0800

    save

--- tests/MigaTest/Core/IUnitTestContext.cs
+++ tests/MigaTest/Core/IUnitTestContext.cs
@@ -1,7 +1,28 @@
-﻿namespace Acorisoft.FutureGL.MigaTest.Core
+﻿using System.Reflection;
+
+namespace Acorisoft.FutureGL.MigaTest.Core
 {
     public interface IUnitTestContext
     {
         UnitTestCase Case { get; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        Type SpecifiedCase { get; }
+        
+        /// <summary>
+        /// 测试实例，实例一旦生成就无法更改。
+        /// </summary>
+        object Instance { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        Type InstanceType { get; set; }
+        
+        IEnumerable<FieldInfo> Fields { get; }
+        IEnumerable<MethodInfo> Methods { get; }
+        IEnumerable<PropertyInfo> Properties { get; }
     }
 }
--- tests/MigaTest/Core/UnitTestRunner.cs
+++ tests/MigaTest/Core/UnitTestRunner.cs
@@ -1,4 +1,6 @@
-﻿namespace Acorisoft.FutureGL.MigaTest.Core
+﻿using System.Reflection;
+
+namespace Acorisoft.FutureGL.MigaTest.Core
 {
     public static class UnitTestRunner
     {
@@ -26,6 +28,43 @@
         private static void testViewModel(IUnitTestContext context)
         {
             
+        }
+
+        private static void PropertyStateCheck(IUnitTestContext context)
+        {
+            //
+            // 检查应该为NULL
+            //
+            // AssertPropertyWasNull
+            // AssertPropertyWasNotNull
+            // AssertPropertyInRange
+            
+            foreach (var property in context.Properties)
+            {
+                if (!property.IsDefined(typeof(Property)))
+                {
+                    continue;
+                }
+            }
+        }
+        
+        
+        private static void FieldStateCheck(IUnitTestContext context)
+        {
+            
+        }
+        
+        private static void ConstructorProcedureCheck(IUnitTestContext context)
+        {
+            // 生成对象
+            context.Instance = Activator.CreateInstance(context.InstanceType);
+            
+            
+            // 检查属性
+            PropertyStateCheck(context);
+
+            // 检查字段
+            FieldStateCheck(context);
         }
     }
 }
