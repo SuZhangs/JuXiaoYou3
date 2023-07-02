1cd15aca9024c0719d633224c94e12458b31280e
Acoris <731984870@qq.com>
2023-07-02 10:14:59 +0800

    Update Version.cs

--- frameworks/MigaStudio.Version/Version.cs
+++ frameworks/MigaStudio.Version/Version.cs
@@ -5,7 +5,7 @@
 [assembly: AssemblyTitle("橘小柚")]
 [assembly: AssemblyVersion("3.0.1.*")]
 [assembly: AssemblyFileVersion("3.0.1.*")]
-[assembly: AssemblyInformationalVersion("3.0.1")]
+[assembly: AssemblyInformationalVersion("3.0.1+Hotfix")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("zh")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
